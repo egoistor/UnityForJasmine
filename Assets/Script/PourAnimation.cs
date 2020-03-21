d6cc9cbac8c4e9dda9e9d171bab1ee166ca390b5
MSuperficial <252829652@qq.com>
2020-03-21 10:11:32 +0800

    add pouring animation

---
+++ Assets/Script/PourAnimation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PourAnimation : MonoBehaviour
+{
+    public bool startToDetect = false;
+    private Ray ray;
+    private RaycastHit hitInfo;
+    private Animation anim;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(startToDetect && Input.GetMouseButtonUp(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hitInfo))
+            {
+                if(hitInfo.collider.gameObject.name.Contains("Basket"))
+                {
+                    anim =  hitInfo.collider.GetComponent<Animation>();
+                    anim.Play();
+                    startToDetect = false;
+                }
+            }
+        }
+        if(anim != null && anim.isPlaying == false)
+        {
+            startToDetect = true;
+            anim = null;
+        }
+    }
+}
